a9de2f78d35eb8c392391df63feb1025fdcdb956
Shadowjeager <bienvenuobombi@gmail.com>
2025-08-15 18:21:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "242056401712",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "",
+OWNER_NAME: process.env.OWNER_NAME || "꧁☫࿐ʟᴏʀᴅ ʀᴇᴀᴘᴇʀ ɢᴏʟᴅᴡɪɴᴅ",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*",
 // add bot owner name    
